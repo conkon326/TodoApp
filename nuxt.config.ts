31510198011d9ca5a08124f3d16e4f2baa112120
con <128914799+conkon326@users.noreply.github.com>
2023-08-27 15:00:54 +0900

    Update nuxt.config.ts
    不要なコードを削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   css: [
     "bootstrap/dist/css/bootstrap.min.css",
-    "bootstrap-vue-3/dist/bootstrap-vue-3.css",
   ],
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
