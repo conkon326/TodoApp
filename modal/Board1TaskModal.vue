507e239c2a9106ea3f0833465e170698004872fd
con <128914799+conkon326@users.noreply.github.com>
2023-08-30 14:02:14 +0900

    Add modalFolder

---
+++ modal/Board1TaskModal.vue
@@ -0,0 +1,148 @@
+<template>
+  <!-- モーダルが開いている場合に表示 -->
+  <div v-if="isModalOpen" class="modal">
+    <div class="modal-content">
+      <h2>ボード1の新しいタスクを作成</h2>
+      <!-- タスク作成フォーム -->
+      <form @submit.prevent="createTask()" class="needs-validation">
+        <div class="form-group">
+           <label for="taskTitle" class="form-label"
+            >タスク名:
+            <span class="form-text">
+              <span class="required-marker">※</span>必須項目です</span>
+          </label>
+           <input
+            type="text"
+            id="taskTitle"
+            v-model="taskTitle"
+            class="form-control"
+            placeholder="タスク名を入力してください"
+            required
+          />
+          <div class="invalid-feedback">タスク名を入力してください</div>
+        </div>
+        <!-- タグ選択ボタン -->
+        <div class="form-group">
+          <label for="taskTagsInput" class="form-label"
+            >タグ:<span class="form-text">タグは複数選択可能です</span>
+            </label>
+          <select
+            class="form-select"
+            multiple
+            v-model="selectedTags"
+            id="taskTagsInput"
+          >
+            <option
+              v-for="tag in taskTags.tags"
+              :key="tag.tagId"
+              :value="tag.tagId"
+            >
+              {{ tag.tag }}
+            </option>
+          </select>
+        </div>
+        <!-- タスク作成ボタン -->
+        <button type="submit" class="btn btn-primary">タスクを作成</button>
+        <!-- モーダルを閉じるボタン -->
+        <button @click="closeModal" class="btn btn-secondary">閉じる</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { useBoard1Task } from "~/stores/tasks";
+import { useTaskTags } from "~/stores/tags";
+export default {
+  props: {
+    isModalOpen: Boolean,
+  },
+  data() {
+    return {
+      taskTitle: "",
+      selectedTags: [],
+    };
+  },
+  methods: {
+    // モーダルを閉じるメソッド
+    closeModal() {
+      this.$emit("close");
+    },
+    // タスクを作成するメソッド
+    createTask() {
+      // if (this.taskTitle.trim() === "") {
+      //   // タグ名が空の場合は何もしない
+      //   return;
+      // }
+      const board1Task = useBoard1Task();
+      // 一意タスクIDを作成するメソッド
+      const generateUniqueTaskId = () => {
+        const currentTaskCount = board1Task.tasks.length;
+        return currentTaskCount + 1;
+      };
+      // 新しいタスクオブジェクトを作成
+      const newTask = {
+        no: generateUniqueTaskId(),
+        title: this.taskTitle,
+        tagIds: this.selectedTags,
+        boardid: 1,
+      };
+      // ストアにタスクを追加
+      board1Task.addTask(newTask);
+      // 入力フィールドをクリアし、モーダルを閉じる
+      this.taskTitle = "";
+      this.selectedTags = [];
+      this.closeModal();
+    },
+  },
+  //タグ名を取得
+  computed: {
+    taskTags() {
+      return useTaskTags();
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* モーダル背景 */
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+/* モーダル */
+.modal-content {
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  width: auto;
+}
+.modal-content {
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  width: auto;
+}
+.required-marker {
+  color: red;
+}
+
+#taskTitle {
+  margin-bottom: 10px;
+}
+
+#taskTagsInput {
+  margin-bottom: 10px;
+}
+
+.btn {
+  margin-right: 10px;
+}
+</style>
---
+++ modal/Board4TaskModal.vue
@@ -0,0 +1,131 @@
+<template>
+  <div v-if="isModalOpen" class="modal">
+    <div class="modal-content">
+      <h2>ボード4の新しいタスクを作成</h2>
+      <form @submit.prevent="createTask()" class="needs-validation">
+        <div class="form-group">
+          <label for="taskTitle" class="form-label"
+            >タスク名:
+            <span class="form-text">
+              <span class="required-marker">※</span>必須項目です</span>
+          </label>
+          <input
+            type="text"
+            id="taskTitle"
+            v-model="taskTitle"
+            class="form-control"
+            placeholder="タスク名を入力してください"
+            required
+          />
+
+          <div class="invalid-feedback">タスク名は必須です。</div>
+        </div>
+        <div class="form-group">
+          <label for="taskTagsInput" class="form-label"
+            >タグ:<span class="form-text">タグは複数選択可能です</span>
+            </label>
+          <select
+            class="form-select"
+            multiple
+            v-model="selectedTags"
+            id="taskTagsInput"
+          >
+            <option
+              v-for="tag in taskTags.tags"
+              :key="tag.tagId"
+              :value="tag.tagId"
+            >
+              {{ tag.tag }}
+            </option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary">タスクを作成</button>
+        <button @click="closeModal" class="btn btn-secondary">閉じる</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { useBoard4Task } from "~/stores/tasks";
+import { useTaskTags } from "~/stores/tags";
+export default {
+  props: {
+    isModalOpen: Boolean,
+  },
+  data() {
+    return {
+      taskTitle: "",
+      selectedTags: [],
+    };
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+    createTask() {
+      if (this.taskTitle.trim() === "") {
+        // タグ名が空の場合は何もしない
+        return;
+      }
+      const board4Task = useBoard4Task();
+      const generateUniqueTaskId = () => {
+        const currentTaskCount = board4Task.tasks.length;
+        return currentTaskCount + 1;
+      };
+
+      const newTask = {
+        no: generateUniqueTaskId(),
+        title: this.taskTitle,
+        tagIds: this.selectedTags,
+        boardid: 1,
+      };
+
+      board4Task.addTask(newTask);
+      this.taskTitle = "";
+      this.selectedTags = [];
+      this.closeModal();
+    },
+  },
+  computed: {
+    taskTags() {
+      return useTaskTags();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal-content {
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  width: auto;
+}
+.required-marker {
+  color: red;
+}
+
+#taskTitle {
+  margin-bottom: 10px;
+}
+
+#taskTagsInput {
+  margin-bottom: 10px;
+}
+
+.btn {
+  margin-right: 10px;
+}
+</style>
